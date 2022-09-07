5bdfa30a392122cc0fea5558e6bfe2a0cadcf30d
kimyubi <ieieie0419@gmail.com>
2022-09-07 20:09:40 +0900

    HTTP Status Code 제어

--- src/main/java/com/example/restfulService/controller/UserController.java
+++ src/main/java/com/example/restfulService/controller/UserController.java
@@ -3,10 +3,11 @@ package com.example.restfulService.controller;
 import com.example.restfulService.UserService;
 import com.example.restfulService.domain.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,4 +25,15 @@ public class UserController {
         return userService.findById(id);
     }
 
+    @PostMapping("/users")
+    public ResponseEntity createUser(@RequestBody User user){
+        User savedUser = userService.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
 }
